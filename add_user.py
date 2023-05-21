53ab674055ef10e51534c308675c5c67a5945172
ayael01 <ayael01@gmail.com>
2023-05-21 22:35:48 +0300

    add users tool

---
+++ add_user.py
@@ -0,0 +1,14 @@
+from app import app, db
+from werkzeug.security import generate_password_hash
+from app import User
+
+# Generate a hashed password
+hashed_password = generate_password_hash('123456', method='sha256')
+
+with app.app_context():   # Create an application context
+    # Create a new user
+    new_user = User(username='Eli', email='ayael01@gmail.com', password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+
+print("User added successfully!")
