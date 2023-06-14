097eae5f360f400a9dddb5175d8dba41edc2d8f2
ayael01 <ayael01@gmail.com>
2023-06-14 19:44:29 +0300

    Running according to environment

--- app.py
+++ app.py
@@ -313,4 +313,9 @@ def add_header(response):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all() # Create database tables
-    app.run(debug=True)
+    
+    if os.environ.get('CHAMPAY_ENV') == 'production':
+        app.run(host='0.0.0.0', port=5000, debug=False)
+    else:
+        app.run(debug=True)
+
