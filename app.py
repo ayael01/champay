34a44fb925e7d3a97b9c6e84d6795d3f37c2c322
ayael01 <ayael01@gmail.com>
2023-06-14 19:39:18 +0300

    set db URI according to environment

--- app.py
+++ app.py
@@ -7,11 +7,17 @@ from flask import render_template_string
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Boolean
 from flask import abort
+import os
 
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://aya_el01:ccaa00@localhost/champay_db'
+
+if os.environ.get('CHAMPAY_ENV') == 'production':
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres123456@champay-rds.crlez4n4tsbc.eu-north-1.rds.amazonaws.com/champay_db'
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://aya_el01:ccaa00@localhost/champay_db'
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
