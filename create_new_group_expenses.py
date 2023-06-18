bd20045355d40d7fe1bec3998a4ee5bf4b5a9ef5
ayael01 <ayael01@gmail.com>
2023-06-18 21:40:30 +0300

    Create group with user input

--- create_new_group_expenses.py
+++ create_new_group_expenses.py
@@ -1,7 +1,12 @@
 from group_expenses_tool import create_group_expenses, app
 
-group_name = "HH in Shuli Lutzi, TA 10/02/23"
-user_ids = [1, 2, 3, 4, 5, 6, 7]
+group_name = input("Please enter a group name: ")
+
+# Input user ids as comma-separated values
+user_ids_string = input("Please enter user IDs separated by commas: ")
+
+# Convert user_ids_string to a list of integers
+user_ids = [int(id_str) for id_str in user_ids_string.split(',')]
 
 result = create_group_expenses(app, group_name, user_ids)
 print(result)
