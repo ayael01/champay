722a921d6ed291a3570196b145414d13a8b49728
ayael01 <ayael01@gmail.com>
2023-07-18 23:19:07 +0300

    Initialize alembic

---
+++ migrations/versions/e5cadd6ed0bd_baseline.py
@@ -0,0 +1,24 @@
+"""Baseline
+
+Revision ID: e5cadd6ed0bd
+Revises: 
+Create Date: 2023-07-18 23:03:40.776478
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e5cadd6ed0bd'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
