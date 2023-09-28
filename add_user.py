643a16699db692acca021149071421cde2ce23cd
ayael01 <ayael01@gmail.com>
2023-09-28 15:29:11 +0300

    Adding ExtraGm user

--- add_user.py
+++ add_user.py
@@ -7,8 +7,8 @@ hashed_password = generate_password_hash('123456', method='sha256')
 
 with app.app_context():   # Create an application context
     # Create a new user
-    new_user = User(username='Champay', email='champay1969@gmail.com', password=hashed_password)
+    new_user = User(username='ExtraGm', email='extragm7@gmail.com', password=hashed_password)
     db.session.add(new_user)
     db.session.commit()
 
-print("User Champay added successfully!")
+print("User ExtraGm added successfully!")
